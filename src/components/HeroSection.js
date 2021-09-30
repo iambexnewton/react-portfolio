e9b95d80ec7469a4526e178e730e973aa1b6ef8a
Becky Newton <iambexnewton@gmail.com>
2021-09-30 19:16:08 +0200

    css for hero section started

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -21,13 +21,7 @@ function HeroSection({
         className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}
       >
         <div className="container">
-          <div
-            className="row home__hero-row"
-            style={{
-              display: 'flex',
-              flexDirection: imgStart === 'start' ? 'row-reverse' : 'row',
-            }}
-          >
+          <div className="row home__hero-row">
             <div className="col">
               <div className="home__hero-text-wrapper">
                 <div className="top-line">{topLine}</div>
--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -4,12 +4,12 @@ import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
 
 function Homepage() {
   return (
-    <>
+    <main className="main">
       <HeroSection {...homeObjOne} />
       <HeroSection {...homeObjTwo} />
       <HeroSection {...homeObjThree} />
       <HeroSection {...homeObjFour} />
-    </>
+    </main>
   );
 }
 
